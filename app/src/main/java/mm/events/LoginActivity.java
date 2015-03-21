5de553a396eca6584d75db22ef24c8feb778f3b3
fe7 <faizanjoya@gmail.com>
2015-03-21 20:58:29 +0000

    notification dummy done, after fb login attempt, some redundant code for login activity

--- app/src/main/java/mm/events/LoginActivity.java
+++ app/src/main/java/mm/events/LoginActivity.java
@@ -1,39 +1,85 @@
 package mm.events;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends FragmentActivity {
+
+    private LoginFragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
+       /* showHashKey(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new LoginFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, mainFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (LoginFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
         }
 
-        return super.onOptionsItemSelected(item);
+        LoginButton authButton = (LoginButton) findViewById(R.id.authButton);
+        authButton.setFragment(mainFragment);
+        authButton.setReadPermissions(Arrays.asList("public_profile"));
+*/
+    }
+
+    public void test(View v) {
+        new Request(
+                Session.getActiveSession(),
+                "/me/events",
+                null,
+                HttpMethod.GET,
+                new Request.Callback() {
+                    public void onCompleted(Response response) {
+                        Log.e("response", response.toString());
+                    }
+                }
+        ).executeAsync();
+    }
+
+    public static void showHashKey(Context context) {
+        Log.e("hash", "I am here");
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    "mm.events", PackageManager.GET_SIGNATURES); //Your            package name here
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) { e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) { e.printStackTrace();
+        }
     }
 }
